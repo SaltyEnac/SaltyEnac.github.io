3055c48e6f99f2af3d415a9302f57a63bc05d505
Mason Cedric Yau <92760945+SaltyEnac@users.noreply.github.com>
2021-10-22 11:23:54 -1000

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,16 @@ function submit(){
   var hello = document.getElementById('select').value
   window.alert(hello)
 }
+
+var a;
+
+function getName(){
+  //get value from input field and save as "a"
+  a = document.getElementById('username').value;
+  //create LS key with value of "a"
+  localStorage.setItem('uName',a);
+  //automatically take user to two.html
+  location.href = "two.html";
+}
+
+
